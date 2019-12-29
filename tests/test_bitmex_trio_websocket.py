a109c0556feadba87591b35989c069d5c9b0eebd
Anders Ellenshøj Andersen <andersa@ellenshoej.dk>
2019-12-29 10:32:54 +0100

    First tests

--- tests/test_bitmex_trio_websocket.py
+++ tests/test_bitmex_trio_websocket.py
@@ -2,24 +2,46 @@
 # -*- coding: utf-8 -*-
 
 """Tests for `bitmex_trio_websocket` package."""
+import os
 
-import pytest
+from trio import sleep
+from random import random
+from trio_websocket import ConnectionRejected, WebSocketConnection, ConnectionClosed
+from bitmex_trio_websocket import connect
 
+async def test_auth_fail():
+    await sleep(random())
+    try:
+        async for msg in connect('testnet', None, 'abcd1234', 'efgh5678'):
+            assert False
+    except ConnectionRejected:
+        assert True
 
-from bitmex_trio_websocket import BitMEXWebsocket
+async def test_auth_success():
+    await sleep(random())
+    try:
+        messages = connect('testnet', None, os.getenv('TESTNET_API_KEY'), os.getenv('TESTNET_API_SECRET'))
+        ws = await messages.asend(None)
+        assert isinstance(ws, WebSocketConnection)
+        async for msg in messages:
+            assert isinstance(msg, dict)
+            assert 'action' in msg
+            await ws.aclose()
 
+    except ConnectionClosed:
+        assert True
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+async def test_multisymbol():
+    await sleep(random())
+    try:
+        messages = connect('testnet', ['XBTUSD', 'ETHUSD'], os.getenv('TESTNET_API_KEY'), os.getenv('TESTNET_API_SECRET'))
+        count = 0
+        ws = await messages.asend(None)
+        async for msg in messages:
+            assert isinstance(msg, dict)
+            count += 1
+            if count >= 100:
+                print(count)
+                await ws.close()
+    except ConnectionClosed:
+        assert True
