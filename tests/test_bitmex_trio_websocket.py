9142f0b81c61549fc8b77915881e89ad921e5fd8
Anders Ellenshøj Andersen <andersa@ellenshoej.dk>
2020-02-02 22:20:26 +0100

    Fixed tests for dynamic channel subscriptions

--- tests/test_bitmex_trio_websocket.py
+++ tests/test_bitmex_trio_websocket.py
@@ -3,30 +3,34 @@
 
 """Tests for `bitmex_trio_websocket` package."""
 import os
-
-from trio import sleep
 from random import random
+
+from trio import sleep, open_nursery
+import ujson
 from trio_websocket import ConnectionRejected, WebSocketConnection, ConnectionClosed
 from bitmex_trio_websocket import BitMEXWebsocket, connect
 
 async def test_auth_fail():
     await sleep(random())
     try:
-        async for msg in connect('testnet', None, None, 'abcd1234', 'efgh5678'):
-            assert False
+        async with open_nursery() as nursery:
+            async for msg in connect(nursery, 'testnet', 'abcd1234', 'efgh5678'):
+                assert False
     except ConnectionRejected:
         assert True
 
 async def test_auth_success():
     await sleep(random())
     try:
-        messages = connect('testnet', None, None, os.getenv('TESTNET_API_KEY'), os.getenv('TESTNET_API_SECRET'))
-        ws = await messages.asend(None)
-        assert isinstance(ws, WebSocketConnection)
-        async for msg in messages:
-            assert isinstance(msg, dict)
-            assert 'action' in msg
-            await ws.aclose()
+        async with open_nursery() as nursery:
+            messages = connect(nursery, 'testnet', os.getenv('TESTNET_API_KEY'), os.getenv('TESTNET_API_SECRET'))
+            ws = await messages.__anext__()
+            assert isinstance(ws, WebSocketConnection)
+            await ws.send_message(ujson.dumps({'op': 'subscribe', 'args': ['margin', 'position', 'order', 'execution']}))
+            async for msg in messages:
+                assert isinstance(msg, dict)
+                assert 'action' in msg
+                await ws.aclose()
 
     except ConnectionClosed:
         assert True
@@ -34,24 +38,31 @@ async def test_auth_success():
 async def test_multisymbol():
     await sleep(random())
     try:
-        messages = connect('testnet', ['XBTUSD', 'ETHUSD'], None, os.getenv('TESTNET_API_KEY'), os.getenv('TESTNET_API_SECRET'))
-        count = 0
-        ws = await messages.asend(None)
-        async for msg in messages:
-            assert isinstance(msg, dict)
-            count += 1
-            if count >= 10:
-                print(count)
-                await ws.aclose()
+        async with open_nursery() as nursery:
+            messages = connect(nursery, 'testnet', os.getenv('TESTNET_API_KEY'), os.getenv('TESTNET_API_SECRET'))
+            count = 0
+            ws = await messages.__anext__()
+            await ws.send_message(ujson.dumps({'op': 'subscribe', 'args': ['instrument:XBTUSD', 'instrument:ETHUSD']}))
+            async for msg in messages:
+                assert isinstance(msg, dict)
+                count += 1
+                if count >= 3:
+                    print(count)
+                    await ws.aclose()
     except ConnectionClosed:
         assert True
 
 async def test_context_manager():
-    async with BitMEXWebsocket.connect('testnet', ['XBTUSD', 'ETHUSD'], ['instrument'], os.getenv('TESTNET_API_KEY'), os.getenv('TESTNET_API_SECRET')) as bitmex_ws:
+    async with BitMEXWebsocket.connect('testnet', os.getenv('TESTNET_API_KEY'), os.getenv('TESTNET_API_SECRET')) as bitmex_ws:
         count = 0
-        async for msg in bitmex_ws.listen('instrument'):
+        async for msg in bitmex_ws.listen('instrument', 'XBTUSD'):
             count += 1
-            print(count)
-            if count >= 10:
+            if count >= 3:
                 break
         assert True
+
+async def test_orderbook():
+    async with BitMEXWebsocket.connect('testnet') as bws:
+        async for msg in bws.listen('orderBookL2', 'XBTUSD'):
+            assert len(msg) == 2
+            break
