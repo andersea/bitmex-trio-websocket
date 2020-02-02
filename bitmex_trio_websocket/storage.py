5a97e96010e2a41abc5e305d8e64c5060f873364
Anders Ellenshøj Andersen <andersa@ellenshoej.dk>
2020-02-02 22:07:50 +0100

    Completely reworked storage using SortedContainers

--- bitmex_trio_websocket/storage.py
+++ bitmex_trio_websocket/storage.py
@@ -1,7 +1,10 @@
+from collections import defaultdict
 from datetime import datetime, timezone
 import decimal
 import logging
 import typing
+
+from sortedcontainers import SortedDict
 
 logger = logging.getLogger(__name__)
 
@@ -11,7 +14,10 @@ class Storage:
     MAX_TABLE_LEN = 200
 
     def __init__(self):
-        self.data = {}
+        self.data = defaultdict(SortedDict)
+        # Special storage for orderBookL2
+        # dict[symbol][side][id]
+        self.orderbook = defaultdict(lambda: defaultdict(SortedDict))
         self.keys = {}
 
     async def process(self, ws):
@@ -26,9 +32,6 @@ class Storage:
 
             logger.debug('Received %s message for table %s.', action, table)
 
-            if table not in self.data:
-                self.data[table] = []
-
             if table not in self.keys:
                 self.keys[table] = []
 
@@ -39,91 +42,99 @@ class Storage:
             # 'delete'  - delete row
             if action == 'partial':
                 logger.debug('%s: partial', table)
-                self.data[table] += message['data']
                 # Keys are communicated on partials to let you know how to uniquely identify
                 # an item. We use it for updates.
                 self.keys[table] = message['keys']
 
-                for item in message['data']:
-                    if 'symbol' in item:
-                        yield (item, item['symbol'], table, action)
-                    else:
-                        yield (item, None, table, action)
+                if table =='orderBookL2':
+                    symbols = set()
+                    for item in message['data']:
+                        symbols.add(item['symbol'])
+                        self.orderbook[item['symbol']][item['side']][item['id']] = item
+                    for symbol in symbols:
+                        yield (self.orderbook[symbol], symbol, table, action)
+                else:
+                    for item in message['data']:
+                        # Store item by key
+                        self.data[table][self.make_key(table, item)] = item
+                        if 'symbol' in item:
+                            yield (item, item['symbol'], table, action)
+                        else:
+                            yield (item, None, table, action)
 
             elif action == 'insert':
                 logger.debug('%s: inserting %s', table, message["data"])
-                self.data[table] += message['data']
 
                 # Limit the max length of the table to avoid excessive memory usage.
                 if table == 'order':
                     # Trim closed orders that are older than a minute - Filled orders
                     # can be reopened by amending leavesQty within a minute. After that
                     # we can delete them.
-                    self.data[table] = [
-                        i for i in self.data[table]
+                    outdated_keys = [
+                        k for k, i in self.data[table].items()
                         if i['leavesQty'] <= 0 and (datetime.now(timezone.utc) - parse_timestamp(i['timestamp'])).total_seconds() > 60
                     ]
+                    for key in outdated_keys:
+                        del self.data[table][key]
                 elif table == 'orderBookL2':
                     # Don't trim the order book because we'll lose valuable state if we do.
                     pass
                 elif len(self.data[table]) > self.MAX_TABLE_LEN:
-                    self.data[table] = self.data[table][(self.MAX_TABLE_LEN // 2):]
+                    # Delete the first half of the keys
+                    del self.data[table].keys()[:(self.MAX_TABLE_LEN // 2)]
 
-                for item in message['data']:
-                    if 'symbol' in item:
+                # Insert items
+                if table == 'orderBookL2':
+                    for item in message['data']:
+                        self.orderbook[item['symbol']][item['side']][item['id']] = item
                         yield (item, item['symbol'], table, action)
-                    else:
-                        yield (item, None, table, action)
+                else:
+                    for item in message['data']:
+                        self.data[table][self.make_key(table, item)] = item
+                        if 'symbol' in item:
+                            yield (item, item['symbol'], table, action)
+                        else:
+                            yield (item, None, table, action)
+
 
             elif action == 'update':
                 logger.debug('%s: updating %s', table, message["data"])
-                # Locate the item in the collection and update it.
-                for updateData in message['data']:
-                    item = self.find_item(table, updateData)
-                    if not item:
-                        continue  # No item found to update. Could happen before push
+                for update in message['data']:
+                    try:
+                        if table == 'orderBookL2':
+                            item = self.orderbook[update['symbol']][update['side']][update['id']]
+                        else:
+                            item = self.data[table][self.make_key(table, update)]
 
-                    # Update this item.
-                    item.update(updateData)
+                        # Update this item.
+                        item.update(update)
 
-                    # Send back the updated item
-                    if 'symbol' in item:
-                        yield (item, item['symbol'], table, action)
-                    else:
-                        yield (item, None, table, action)
+                        # Send back the updated item
+                        if 'symbol' in item:
+                            yield (item, item['symbol'], table, action)
+                        else:
+                            yield (item, None, table, action)
+                    except KeyError:
+                        continue # No item found to update. Could happen before push
 
             elif action == 'delete':
                 logger.debug('%s: deleting %s', table, message["data"])
-                # Locate the item in the collection and remove it.
-                for deleteData in message['data']:
-                    item = self.find_item(table, deleteData)
-                    self.data[table].remove(item)
+                for deleted in message['data']:
+                    # Locate the item in the collection and remove it.
+                    try:
+                        if table == 'orderBookL2':
+                            del self.orderbook[deleted['symbol']][deleted['side']][deleted['id']]
+                        else:
+                            del self.data[table][self.make_key(table, deleteData)]
+                    except KeyError:
+                        pass # Item not found
             else:
                 raise Exception("Unknown action: %s" % action)
 
             logger.debug('Next iteration.')
-
-    def get_instrument(self, symbol):
-        instruments = self.data['instrument']
-        matchingInstruments = [i for i in instruments if i['symbol'] == symbol]
-        if not matchingInstruments:
-            raise Exception("Unable to find instrument or index with symbol: " + symbol)
-        instrument = matchingInstruments[0]
-        # Turn the 'tickSize' into 'tickLog' for use in rounding
-        # http://stackoverflow.com/a/6190291/832202
-        instrument['tickLog'] = decimal.Decimal(str(instrument['tickSize'])).as_tuple().exponent * -1
-        return instrument
-
-    def find_item(self, table: str, match_data: typing.Mapping[str, typing.Union[str, int, float]]):
-        keys = self.keys[table]
-        records = self.data[table]
-        for item in records:
-            matched = True
-            for key in keys:
-                if item[key] != match_data[key]:
-                    matched = False
-            if matched:
-                return item
+    
+    def make_key(self, table, match_data):
+        return tuple(match_data[key] for key in self.keys[table])
     
     @staticmethod
     def parse_timestamp(timestamp: str) -> datetime:
