f91b1bfc7d71c05cc6e90b4a14663dc9302b1113
Anders Ellenshøj Andersen <andersa@ellenshoej.dk>
2020-02-05 20:50:31 +0100

    Bump version

--- bitmex_trio_websocket/__init__.py
+++ bitmex_trio_websocket/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Anders Ellenshøj Andersen"""
 __email__ = 'andersa@atlab.dk'
-__version__ = '0.4.1'
+__version__ = '0.4.2'
 
 from collections import defaultdict
 from contextlib import asynccontextmanager
