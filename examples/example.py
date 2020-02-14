a1a54a9e66a4b49cf36c69f89c23d5ba03d21457
Anders Ellenshøj Andersen <andersa@ellenshoej.dk>
2020-02-14 22:26:06 +0100

    update examples

--- examples/example.py
+++ examples/example.py
@@ -7,12 +7,12 @@ sys.path.append(os.getcwd() + '/..')
 import trio
 from async_generator import aclosing
 
-from bitmex_trio_websocket import BitMEXWebsocket
+from bitmex_trio_websocket import open_bitmex_websocket
 
 logging.basicConfig(level='INFO', format='[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s')
 
 async def main():
-    async with BitMEXWebsocket.connect('testnet') as bws:
+    async with open_bitmex_websocket('testnet') as bws:
         count = 0
         async with aclosing(bws.listen('instrument')) as agen:
             async for msg in agen:
--- examples/example2.py
+++ examples/example2.py
@@ -7,18 +7,18 @@ sys.path.append(os.getcwd() + '/..')
 import trio
 from async_generator import aclosing
 
-from bitmex_trio_websocket import BitMEXWebsocket
+from bitmex_trio_websocket import open_bitmex_websocket
 
 logging.basicConfig(level='INFO', format='[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s')
 
 async def main():
-    async with BitMEXWebsocket.connect('testnet') as bws:
+    async with open_bitmex_websocket('testnet') as bws:
         count = 0
-        async with aclosing(bws.listen('tradeBin1h', 'XBTM20')) as agen:
+        async with aclosing(bws.listen('orderBookL2', 'XBTUSD')) as agen:
             async for msg in agen:
-                print(f'Received message, symbol: \'{msg["symbol"]}\', timestamp: \'{msg["timestamp"]}\'')
+                print(f'Received message, symbol: \'{msg["symbol"]}\'')
                 count += 1
-                if count == 5:
+                if count == 100:
                     break
 
 
