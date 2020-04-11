2c9f06b189e54c0ea3d314f5bbd02e5642a1fee8
Anders Ellenshøj Andersen <andersa@ellenshoej.dk>
2020-04-11 23:50:33 +0200

    Fix tests for connection closure propagation

--- tests/test_bitmex_trio_websocket.py
+++ tests/test_bitmex_trio_websocket.py
@@ -24,27 +24,33 @@ async def test_auth_fail():
 
 async def test_auth_success():
     bitmex_websocket = BitMEXWebsocket()
-    async with bitmex_websocket._connect('testnet', os.getenv('TESTNET_API_KEY'), os.getenv('TESTNET_API_SECRET'), False):
-        async with aclosing(bitmex_websocket._websocket_parser()) as agen:
-            assert isinstance(bitmex_websocket._ws, WebSocketConnection)
-            await bitmex_websocket._ws.send_message(ujson.dumps({'op': 'subscribe', 'args': ['margin', 'position', 'order', 'execution']}))
-            async for msg in agen:
-                assert isinstance(msg, dict)
-                assert 'action' in msg
-                await bitmex_websocket._ws.aclose()
+    try:
+        async with bitmex_websocket._connect('testnet', os.getenv('TESTNET_API_KEY'), os.getenv('TESTNET_API_SECRET'), False):
+            async with aclosing(bitmex_websocket._websocket_parser()) as agen:
+                assert isinstance(bitmex_websocket._ws, WebSocketConnection)
+                await bitmex_websocket._ws.send_message(ujson.dumps({'op': 'subscribe', 'args': ['margin', 'position', 'order', 'execution']}))
+                async for msg in agen:
+                    assert isinstance(msg, dict)
+                    assert 'action' in msg
+                    await bitmex_websocket._ws.aclose()
+    except ConnectionClosed as e:
+        assert e.reason.code == 1000
 
 async def test_multisymbol():
     bitmex_websocket = BitMEXWebsocket()
-    async with bitmex_websocket._connect('testnet', os.getenv('TESTNET_API_KEY'), os.getenv('TESTNET_API_SECRET'), False):
-        count = 0
-        async with aclosing(bitmex_websocket._websocket_parser()) as agen:
-            await bitmex_websocket._ws.send_message(ujson.dumps({'op': 'subscribe', 'args': ['instrument:XBTUSD', 'instrument:ETHUSD']}))
-            async for msg in agen:
-                assert isinstance(msg, dict)
-                count += 1
-                if count >= 3:
-                    print(count)
-                    await bitmex_websocket._ws.aclose()
+    try:
+        async with bitmex_websocket._connect('testnet', os.getenv('TESTNET_API_KEY'), os.getenv('TESTNET_API_SECRET'), False):
+            count = 0
+            async with aclosing(bitmex_websocket._websocket_parser()) as agen:
+                await bitmex_websocket._ws.send_message(ujson.dumps({'op': 'subscribe', 'args': ['instrument:XBTUSD', 'instrument:ETHUSD']}))
+                async for msg in agen:
+                    assert isinstance(msg, dict)
+                    count += 1
+                    if count >= 3:
+                        print(count)
+                        await bitmex_websocket._ws.aclose()
+    except ConnectionClosed as e:
+        assert e.reason.code == 1000
 
 async def test_context_manager():
     async with open_bitmex_websocket('testnet', os.getenv('TESTNET_API_KEY'), os.getenv('TESTNET_API_SECRET')) as bitmex_ws:
