f490b8b5d681b40ecab367ae407bfde47eacd6c3
Anders Ellenshøj Andersen <andersa@ellenshoej.dk>
2020-09-05 20:14:00 +0200

    Bump version: 0.8.1 → 0.9.0

--- bitmex_trio_websocket/__init__.py
+++ bitmex_trio_websocket/__init__.py
@@ -4,6 +4,6 @@
 
 __author__ = """Anders Ellenshøj Andersen"""
 __email__ = 'andersa@atlab.dk'
-__version__ = '0.8.1'
+__version__ = '0.9.0'
 
 from .websocket import open_bitmex_websocket, BitMEXWebsocket
